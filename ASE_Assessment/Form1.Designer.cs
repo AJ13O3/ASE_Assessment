4ad9ae3339093e73b255fdeab0c2c3ca0ba45ffc
AJ13O3 <amanjoshua03@gmail.com>
2023-11-08 21:24:03 +0000

    Initial commit

---
+++ ASE_Assessment/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ASE_Assessment
+{
+    partial class GraphicsLanguage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            commandLine = new TextBox();
+            runButton = new Button();
+            drawBox = new PictureBox();
+            programBox = new TextBox();
+            openButton = new Button();
+            saveButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)drawBox).BeginInit();
+            SuspendLayout();
+            // 
+            // commandLine
+            // 
+            commandLine.Location = new Point(12, 356);
+            commandLine.Multiline = true;
+            commandLine.Name = "commandLine";
+            commandLine.Size = new Size(776, 23);
+            commandLine.TabIndex = 0;
+            commandLine.TextChanged += commandLine_TextChanged;
+            commandLine.KeyPress += commandLine_KeyPress;
+            // 
+            // runButton
+            // 
+            runButton.Location = new Point(12, 385);
+            runButton.Name = "runButton";
+            runButton.Size = new Size(75, 23);
+            runButton.TabIndex = 1;
+            runButton.Text = "Run";
+            runButton.UseVisualStyleBackColor = true;
+            runButton.Click += button1_Click;
+            // 
+            // drawBox
+            // 
+            drawBox.BackColor = SystemColors.ControlDark;
+            drawBox.Location = new Point(403, 31);
+            drawBox.Name = "drawBox";
+            drawBox.Size = new Size(385, 319);
+            drawBox.TabIndex = 2;
+            drawBox.TabStop = false;
+            drawBox.Paint += drawBox_Paint;
+            // 
+            // programBox
+            // 
+            programBox.Location = new Point(12, 31);
+            programBox.Multiline = true;
+            programBox.Name = "programBox";
+            programBox.Size = new Size(385, 319);
+            programBox.TabIndex = 3;
+            // 
+            // openButton
+            // 
+            openButton.Location = new Point(403, 385);
+            openButton.Name = "openButton";
+            openButton.Size = new Size(75, 23);
+            openButton.TabIndex = 4;
+            openButton.Text = "Open";
+            openButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            saveButton.Location = new Point(713, 385);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(75, 23);
+            saveButton.TabIndex = 5;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            // 
+            // GraphicsLanguage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(saveButton);
+            Controls.Add(openButton);
+            Controls.Add(programBox);
+            Controls.Add(drawBox);
+            Controls.Add(runButton);
+            Controls.Add(commandLine);
+            Name = "GraphicsLanguage";
+            Text = "GraphicsLanguage";
+            ((System.ComponentModel.ISupportInitialize)drawBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox commandLine;
+        private Button runButton;
+        private PictureBox drawBox;
+        private TextBox programBox;
+        private Button openButton;
+        private Button saveButton;
+    }
+}
---
+++ ASE_Assessment/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASE_Assessment
+{
+    public partial class GraphicsLanguage : Form
+    {
+        private CommandParser commandParser;
+        public GraphicsLanguage()
+        {
+            InitializeComponent();
+            commandParser = new CommandParser(drawBox.CreateGraphics());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            commandParser.processCommand(commandLine.Text);
+            commandLine.Text = null;
+        }
+
+        private void commandLine_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void commandLine_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                commandParser.processCommand(commandLine.Text);
+                commandLine.Text = null;
+            }
+        }
+
+        private void drawBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+        }
+    }
+    public class CommandParser
+    {
+        private Graphics drawingGraphics;
+
+        public CommandParser(Graphics graphics)
+        {
+            drawingGraphics = graphics;
+        }
+
+        public void processCommand(string entry)
+        {
+            entry = entry.ToLower();
+            if (entry.Contains("rectangle"))
+            {
+                string[] parts = entry.Split(' ');
+
+                if (parts.Length == 3 && int.TryParse(parts[1], out int width) && int.TryParse(parts[2], out int height))
+                {
+                    DrawRectangle(width, height);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid parameters for rectangle command.");
+                }
+            }
+        }
+
+        private void DrawRectangle(int width, int height)
+        {
+            Pen pen = new Pen(Color.Black); // You can customize the pen color and other properties here
+            drawingGraphics.DrawRectangle(pen, 10, 10, width, height);
+        }
+    }
+
+}
